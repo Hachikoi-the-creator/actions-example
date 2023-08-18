f61b2e0b7b8f8ead02676871e95ee02b1fa0b4e9
Adan Moreno <adan.moreno@heyhom.mx>
2023-08-18 10:47:47 -0600

    added prettier

--- src/app.tsx
+++ src/app.tsx
@@ -5,33 +5,30 @@ import viteLogo from "/vite.svg";
 import "./app.css";
 
 export const App = component$(() => {
-    const count = useSignal(0);
+  const count = useSignal(0);
 
-    return (
-      <h2>babu</h2>
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} class="logo" alt="Vite logo" />
-                </a>
-                <a href="https://qwik.builder.io" target="_blank">
-                    <img src={qwikLogo} class="logo qwik" alt="Qwik logo" />
-                </a>
-            </div>
-            <h1>Vite + Qwik</h1>
-            <div class="card">
-                <h2>activate workflow</h2>
-                <button onClick$={() => count.value++}>
-                    count is {count.value}
-                </button>
-            </div>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Nesciunt qui earum cupiditate similique?
-            </p>
-            <p class="read-the-docs">
-                Click on the Vite and Qwik logos to learn more
-            </p>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://qwik.builder.io" target="_blank">
+          <img src={qwikLogo} class="logo qwik" alt="Qwik logo" />
+        </a>
+      </div>
+      <h1>Vite + Qwik</h1>
+      <div class="card">
+        <h2>activate workflow</h2>
+        <button onClick$={() => count.value++}>count is {count.value}</button>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui
+        earum cupiditate similique?
+      </p>
+      <p class="read-the-docs">
+        Click on the Vite and Qwik logos to learn more
+      </p>
+    </>
+  );
 });
